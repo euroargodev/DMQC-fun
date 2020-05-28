cb0a754508d74aa17043dd0e452000ab356d02f8
Jan Even Oeie Nilsen <evenrev1@me.com>
2020-05-28 19:40:49 +0200

    Corrected the erroneous change of LONG to negative values west of zero meridian.

--- plot_profiles.m
+++ plot_profiles.m
@@ -1,5 +1,9 @@
+% plotting for load_floats.m
 
-LONG(LONG>180)=LONG(LONG>180)-360;
+%LONG(LONG>180)=LONG(LONG>180)-360;
+% 28.05.2020: Corrected the erroneous change of LONG to negative values west of zero meridian:
+lon=LONG; lon(lon>180)=lon(lon>180)-360;
+
 figure(str2num(char(float_names{I})));clf;
 %set(gcf,'OuterPosition',get(0,'ScreenSize'));
 set(gcf,'OuterPosition',[1 385 1026 960]);
@@ -7,11 +11,11 @@ set(gcf,'OuterPosition',[1 385 1026 960]);
 % Map:
 subplot 231; 
 %title([upper(tartyp{j}(1:end-1)),'-reference data in WMO-square ',int2str(wmosq)]);
-title([nbt,' at float number ',float_names{I}]);
-lim=[mima(LONG) mima(LAT)];
+if exist('nbt'), title([nbt,' at float number ',float_names{I}]); end
+lim=[mima(lon) mima(LAT)];
 m_proj('Albers','lon',lim(1:2)+[-4 4],'lat',lim(3:4)+[-1 1])
 m_grid; m_coast('color','k'); m_elev('contour','color',[.7 .7 .7]);
-wmos=unique(findwmo(LONG,LAT));wmos=wmos(~isnan(wmos));
+wmos=unique(findwmo(lon,LAT));wmos=wmos(~isnan(wmos));
 for ii=1:length(wmos)
   wmolim=wmosquare(wmos(ii)); [lo,la]=erect(wmolim,'t'); m_line(lo,la);
   sort([lim(1:2)+[-4 4],wmolim(1:2)]);lom=ans(2:3);
@@ -20,43 +24,46 @@ for ii=1:length(wmos)
   set(htx,'verticalalignment','middle','horizontalalignment','center','color','b');
 end
 %[lo,la]=erect(lim,'t'); hw=m_line(lo,la,'color','b');
-IA=1:length(LONG); %[ans,IA]=sort(LAT);
-hp=m_scatter(LONG,LAT,5,jet(n));set(hp,'marker','*');
-hpnb=m_line(LONG(jnb),LAT(jnb)); set(hpnb,'marker','o','color','k','linestyle','none');
+IA=1:length(lon); %[ans,IA]=sort(LAT);
+hp=m_scatter(lon,LAT,5,jet(n));set(hp,'marker','*');
+ht=m_text(lon(10:10:end),LAT(10:10:end),int2str(CYCLE_NUMBER(10:10:end)'));
+if exist('jnb'), hpnb=m_line(lon(jnb),LAT(jnb)); set(hpnb,'marker','o','color','k','linestyle','none'); end
 %hl=legend([hw hp(1)],'WMO-square','Reference data','location','northwestoutside');
-hcb=colorbar('southoutside');colormap(jet(n));caxis([0 n]);xlabel(hcb,'Profile number');
+hcb=colorbar('southoutside');colormap(jet(n));caxis([0 n]);
+xlabel(hcb,['Profile number (',datestr(datenum(DATES(1),0,0),12),'-',datestr(datenum(DATES(end),0,0),12),')']);
 
 % TS-diagram:
 subplot 232
 tsdiagrm(mima(SAL),mima(TEMP),0);
 hTS=line(SAL,TEMP,'marker','.','linestyle','none');
-hTSnb=line(SAL(nb),TEMP(nb));%,'marker','o','linestyle','none','color','k');
+if exist('nb'), hTSnb=line(SAL(nb),TEMP(nb)); end %,'marker','o','linestyle','none','color','k');
 set(hTS,{'color'},num2cell(jet(size(PRES,2)),2));
 
 % Profiles:
 subplot 234
 hT=line(PRES,TEMP); 
-hTnb=line(PRES(nb),TEMP(nb)); 
+if exist('nb'), hTnb=line(PRES(nb),TEMP(nb)); end
 view([90 90]);grid;set(gca,'yaxislocation','right');
 set(hT,{'color'},num2cell(jet(size(PRES,2)),2));
 xlabel Pressure; ylabel Temperature
 
 subplot 235
 hS=line(PRES,SAL); 
-hSnb=line(PRES(nb),SAL(nb));
+if exist('nb'), hSnb=line(PRES(nb),SAL(nb)); end
 view([90 90]);grid;set(gca,'yaxislocation','right');
 set(hS,{'color'},num2cell(jet(size(PRES,2)),2));
 xlabel Pressure; ylabel Salinity
 
 subplot 236
 hD=line(PRES,DENS); 
-hDnb=line(PRES(nb),DENS(nb));
+if exist('nb'), hDnb=line(PRES(nb),DENS(nb)); end
 view([90 90]);grid;set(gca,'yaxislocation','right');
 set(hD,{'color'},num2cell(jet(size(PRES,2)),2));
 xlabel Pressure; ylabel Density
 
-set([hTSnb,hTnb,hSnb,hDnb],'color','k','linewidth',2,'linestyle','none','marker','o')
-
+try 
+  set([hTSnb,hTnb,hSnb,hDnb],'color','k','linewidth',2,'linestyle','none','marker','o')
+end
 
 %delete([hTSnb,hTnb,hSnb,hpnb]); % For a clean plot
 
