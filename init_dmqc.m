a7ff229e69db04c0dea6f6dcff802902809e5f01
Jan Even Oeie Nilsen <evenrev1@me.com>
2020-12-11 00:18:18 +0100

    Moved the ftp download option from init_dmqc.m to load_floats.m.

--- init_dmqc.m
+++ init_dmqc.m
@@ -1,14 +1,16 @@
+
+% THE LIST OF FLOATS:
 % The variable float_names is necessary for ow_calibration.m. Do not
 % change name of this variable!
-% THE LIST OF FLOATS:
 float_names={'6903548' '6903549' '6903550' '6903551' '6903553' '6903554' '6903555' '6903557' '6903558' '6903559' '6903560' '6903561' '6903562' '6903563' '6903564'};
 corr       ={ false     false     false     true      false     false     false     false     false     false     false     false     true      false     false   };
 % USE THE FOLLOWING WHILE DOING DMQC WITH OWC: 
-%float_names={'6903561'}; % The one you are working on while DMQC-ing.
+%float_names={'6903550'}; % The one you are working on while DMQC-ing.
 %float_names={'6903549','6903554','6903561','6903548'}; % The 'difficult' ones.
 
 % DOWNLOAD directories:
 download_dir=strcat('~/Downloads/ARGO/',float_names,filesep);
+DIR_FTP=download_dir; % For dm_floats
 download_ref_data_dir='~/Downloads/DMQC';
 % MATLABOW directories:
 owc_data_dir='~/Arkiv/data/matlab_owc/';
@@ -21,18 +23,11 @@ outfiles=strcat(source_dir,float_dirs,float_names,'.mat'); % For load_profiles
 float_dir=[owc_data_dir,'float_source',filesep,'project_NorARGO/'];
 mapped_dir=[owc_data_dir,'float_mapped',filesep,'project_NorARGO/'];
 calib_dir=[owc_data_dir,'float_calib',filesep,'project_NorARGO/'];
-rootdirin=strcat(download_dir,'Rfiles/');
+rootdirin=strcat(download_dir,'profiles/');
 rootdirout=strcat(download_dir,'Dfiles/');
+% Your working directory for ARGO calibration:
+my_working_dir='~/arbeid/obs/NorARGO/';
 
-MAP_P_EXCLUDE=400; % Keep as same as in ow_config.txt but for use in our load_floats.m.
+MAP_P_EXCLUDE=400; % Keep as same as in ow_config.txt but for use in our LOAD_FLOATS.
 
-if logical(0) % ----- SWITCH ON IN ORDER TO DOWNLOAD NEW FLOAT FILES AND R-FILES ------
-  for I=4%1:length(download_dir)
-    mkdir(download_dir{I}); 
-    system(['lftp -e ''lcd ',download_dir{I},' ; get ',float_names{I},'_prof.nc ; bye'' ftp.ifremer.fr/ifremer/argo/dac/coriolis/',float_names{I},'/']);
-    mkdir(rootdirin{I}); 
-    system(['lftp -e ''lcd ',rootdirin{I},' ; mget R',float_names{I},'_*.nc ; bye'' ftp.ifremer.fr/ifremer/argo/dac/coriolis/',float_names{I},'/profiles/']);
-  end
-end % --------------------------------------------------------------------------------
-% R-files must be downloaded at the same time, or else there might be
-% mismatch between number of cal parameters and R-files.
+
